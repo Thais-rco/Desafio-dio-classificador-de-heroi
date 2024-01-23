e82b468b30e508cee6af9c83c3e09966ea901906
thais-rco <thais.rocha2290@gmail.com>
2024-01-22 21:11:20 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let nomeHeroi = "Thais "
+let xp =  1297
+let nivel = "Está no nível "
+
+if (xp <= 1000 ) {
+	console.log("O Heroi de nome " +  nomeHeroi + nivel + "Ferro")
+	}
+
+else if (xp >1000 && xp <= 2000) {
+	console.log( "O Heroi de nome " + nomeHeroi + nivel + "Bronze")
+    }
+    
+else if (xp > 2000 && xp <= 5000) {
+	console.log( "O Heroi de nome " + nomeHeroi + nivel + "Prata")
+    }
+
+else if (xp > 5000 && xp <= 7000){
+	console.log( "O Heroi de nome " + nomeHeroi + nivel + "Ouro")
+    }
+
+
+else if (xp > 7000 && xp <= 8000) {
+	console.log("O Heroi de nome " + nomeHeroi + nivel + "Platina")
+    }
+    
+else if (xp > 8000 && xp <= 9000) {
+	console.log("O Heroi de nome " + nomeHeroi + nivel + "Ascendente")
+    }
+    
+else if (xp > 9000 && xp <=10000) {
+	console.log("O Heroi de nome "  + nomeHeroi + nivel + "Imortal")
+    }
+    
+else {
+	console.log("O Heroi de nome "  + nomeHeroi + nivel + "Radiante")}
+        
